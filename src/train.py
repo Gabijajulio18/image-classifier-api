dcb09dbf710851fb9a7fd50f6ba72cba1d6e21ce
Gabriel Julio <gabijajulio179@gmail.com>
2025-07-25 00:16:29 +0100

    Update parameters

--- src/train.py
+++ src/train.py
@@ -68,14 +68,14 @@ model = keras.Sequential(
         layers.Rescaling(
             1.0 / 255, input_shape=IMG_SIZE + (3,)
         ),  # Normalize pixel values to [0, 1]
-        layers.Conv2D(32, 3, activation="relu"),  # Learn patterns/features from images
+        layers.Conv2D(64, 3, activation="relu"),  # Learn patterns/features from images
         layers.MaxPooling2D(),  # Reduces image size, keeps most important info
-        layers.Conv2D(64, 3, activation="relu"),
-        layers.MaxPooling2D(),
         layers.Conv2D(128, 3, activation="relu"),
         layers.MaxPooling2D(),
+        layers.Conv2D(256, 3, activation="relu"),
+        layers.MaxPooling2D(),
         layers.Flatten(),  # Converts 3D feature to 1D vector
-        layers.Dense(128, activation="relu"),  # Learns higher-level features
+        layers.Dense(256, activation="relu"),  # Learns higher-level features
         layers.Dense(
             num_classes, activation="linear"
         ),  # Outputs probabilities for each class
@@ -93,7 +93,7 @@ model.compile(
 history = model.fit(
     train_ds,
     validation_data=val_ds,
-    epochs=10,
+    epochs=50,
 )
 
 
